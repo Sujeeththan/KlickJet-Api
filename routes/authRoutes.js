a88828304d7fe15c3af19aa297d44fe5eda2c249
sujeeththan <sujeethsujeeth487@gmail.com>
2025-11-04 21:49:14 +0530

    authRoute create

---
+++ routes/authRoutes.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+import {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+} from "../controllers/authController.js";
+
+const authRouter = express.Router();
+
+authRouter.get("/", getAllUsers);
+authRouter.get("/:id", getUserById);
+authRouter.post("/", createUser);
+authRouter.put("/:id", updateUser);
+authRouter.delete("/:id", deleteUser);
+
+export default authRouter;
