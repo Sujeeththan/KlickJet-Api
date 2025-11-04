5fb08e451bfee1704d22332692dc7576cfa28eae
sujeeththan <sujeethsujeeth487@gmail.com>
2025-11-04 21:49:34 +0530

    customerRoute create

---
+++ routes/customerRoutes.js
@@ -0,0 +1,19 @@
+import express from "express"
+
+import{
+    getAllCustomers,
+    getCustomerById,
+    createCustomer,
+    updateCustomer,
+    deleteCustomer
+} from "../controllers/customerController.js"
+
+const  customerRouter = express.Router();
+
+ customerRouter.get("/", getAllCustomers);
+ customerRouter.get("/:id", getCustomerById);
+ customerRouter.post("/", createCustomer);
+ customerRouter.put("/:id", updateCustomer);
+ customerRouter.delete("/:id", deleteCustomer);
+
+export default  customerRouter;
