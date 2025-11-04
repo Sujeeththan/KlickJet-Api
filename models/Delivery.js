651c368d69dc26b9cbcc7e07b8c510da52ad9b05
sujeeththan <sujeethsujeeth487@gmail.com>
2025-11-04 21:47:48 +0530

    delivery schema create

---
+++ models/Delivery.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const deliverySchema = new mongoose.Schema({
+  order: {
+    type: String,
+    ref: "Sale",
+    required: true,
+  },
+  deliveryPerson: {
+    type: String,
+    ref: "DeliveryPerson",
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["pending", "dispatched", "delivered", "cancelled"],
+    default: "pending",
+  },
+  deliveryDate: {
+    type: Date,
+    default: null, 
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+},
+ {
+    timestamps: false,
+    versionKey: false,
+  }
+);
+
+export default mongoose.model("Delivery", deliverySchema);
