b684e0c05b411b9b642a0fe0c9d98e0c7377af30
sujeeththan <sujeethsujeeth487@gmail.com>
2025-11-05 12:20:44 +0530

    authRoute import

--- app.js
+++ app.js
@@ -2,36 +2,34 @@ import express from "express";
 import connectDB from "./config/db.js";
 import customerRouter from "./routes/customerRoutes.js";
 import itemRouter from "./routes/itemRoutes.js";
-import authRoutes from "./routes/authRoutes.js";
+import authRoutes from "./routes/userRoutes.js";
 import deliveryPersonRouter from "./routes/deliveryPersonRoutes.js";
 import deliveryRouter from "./routes/deliveryRoutes.js";
 import orderRouter from "./routes/orderRoutes.js";
 import shopRouter from "./routes/shopRoutes.js";
 import cors from "cors";
 
-const allowedOrigins = []
+const allowedOrigins = [];
 
 const app = express();
 app.use(express.json());
-app.use(cors({origin: allowedOrigins , credentials:true}))
+app.use(cors({ origin: allowedOrigins, credentials: true }));
 
-
-app.get("/", (req,res) => {
-    res.send("My project pos system working")
+app.get("/", (req, res) => {
+  res.send("My project pos system working");
 });
 
 const PORT = process.env.PORT;
 
-connectDB()
+connectDB();
 
 app.use("/api/customers", customerRouter);
 app.use("/api/items", itemRouter);
-app.use("/api/users" , authRoutes);
-app.use("/api/deliveryPerson" , deliveryPersonRouter);
-app.use("/api/delivery" , deliveryRouter)
-app.use("/api/order" ,orderRouter)
-app.use("/api/shop" , shopRouter)
-
+app.use("/api/users", authRoutes);
+app.use("/api/deliveryPerson", deliveryPersonRouter);
+app.use("/api/delivery", deliveryRouter);
+app.use("/api/order", orderRouter);
+app.use("/api/shop", shopRouter);
 
 app.listen(PORT, () =>
   console.log(`Server is running in http://localhost:${PORT}`)
