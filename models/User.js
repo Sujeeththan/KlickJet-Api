20048fd77b5dd058bca627d9da7032504e879954
sujeeththan <sujeethsujeeth487@gmail.com>
2025-11-05 16:21:22 +0530

    userSchema update

--- models/User.js
+++ models/User.js
@@ -1,39 +1,43 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-  },
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Username required"],
+      unique: true,
+      trim: true,
+    },
 
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
+    email: {
+      type: String,
+      required: [true, "Email required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+      match: [/.+\@.+\..+/, "Invalid email"],
+    },
 
-  password: {
-    type: String,
-    required: true,
-  },
+    password: {
+      type: String,
+      required: [true, "Password required"],
+    },
 
-  role: {
-    type: String,
-    enum: ["admin", "cashier"],
-    default: "cashier",
-  },
+    role: {
+      type: String,
+      enum: ["admin", "cashier"],
+      default: "cashier",
+    },
 
-  createdAt: {
-    type: Date,
-    default: Date.now,
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
   },
-},
   {
     timestamps: false,
     versionKey: false,
-    
-});
+  }
+);
 
 export default mongoose.model("User", userSchema);
