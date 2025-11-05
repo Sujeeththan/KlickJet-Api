524f2dd369475904c5b982078ceb9bcc6916cac1
sujeeththan <sujeethsujeeth487@gmail.com>
2025-11-05 22:55:00 +0530

    type change

--- models/Delivery.js
+++ models/Delivery.js
@@ -1,35 +1,36 @@
 import mongoose from "mongoose";
 
-const deliverySchema = new mongoose.Schema({
-  order: {
-    type: String,
-    ref: "Sale",
-    required: true,
+const deliverySchema = new mongoose.Schema(
+  {
+    order: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Order",
+      required: true,
+    },
+    deliverer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Deliverer",
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "dispatched", "delivered", "cancelled"],
+      default: "pending",
+    },
+    deliveryDate: {
+      type: Date,
+      default: null,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  deliveryPerson: {
-    type: String,
-    ref: "DeliveryPerson",
-    required: true,
-  },
-  address: {
-    type: String,
-    required: true,
-  },
-  status: {
-    type: String,
-    enum: ["pending", "dispatched", "delivered", "cancelled"],
-    default: "pending",
-  },
-  deliveryDate: {
-    type: Date,
-    default: null, 
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-},
- {
+  {
     timestamps: false,
     versionKey: false,
   }
