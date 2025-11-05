a537e5dba99ecd031aecb96cbfa091a9373ee3c5
sujeeththan <sujeethsujeeth487@gmail.com>
2025-11-05 16:18:06 +0530

    item schema created

--- models/Item.js
+++ models/Item.js
@@ -1,50 +1,49 @@
 import mongoose from "mongoose";
 
-const itemSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
+const itemSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: [true, "Item name required"], trim: true },
 
-  sku: {
-    type: String,
-    unique: true,
-    trim: true,
-  },
+    sku: {
+      type: String,
+      required: [true, "SKU required"],
+      unique: true,
+      trim: true,
+    },
 
-  description: {
-    type: String,
-    default: null,
-    trim: true,
-  },
+    description: {
+      type: String,
+      default: null,
+      trim: true,
+    },
 
-  price: {
-    type: Number,
-    Request: true,
-    min: 0,
-  },
+    price: {
+      type: Number,
+      required: [true, "Price required"],
+      min: [0, "Price cannot be negative"],
+    },
 
-  stock: {
-    type: Number,
-    default: 0,
-    min: 0,
-  },
+    stock: {
+      type: Number,
+      default: 0,
+      min: 0,
+    },
 
-  category: {
-    type: String,
-    default: null,
-    trim: true,
-  },
+    category: {
+      type: String,
+      default: null,
+      trim: true,
+    },
 
-  createAt: {
-    type: Date,
-    default: Date.now,
+    createAt: {
+      type: Date,
+      default: Date.now,
+    },
   },
-},
-{
+  {
     timestamps: false,
     versionKey: false,
-});
+  }
+);
 
 export default mongoose.model("Item", itemSchema);
